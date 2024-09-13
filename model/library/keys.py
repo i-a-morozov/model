27c0ef6eba6b5ce657736e217493ba5afc9b7335
i-a-morozov <i-a-morozov@users.noreply.github.com>
2024-09-13 11:12:07 +0700

    09_13_2024: updated deviation keys

--- model/library/keys.py
+++ model/library/keys.py
@@ -5,22 +5,33 @@ Keys
 Deviation keys
 
 """
-KEY_DP:str = 'dp'
-KEY_DL:str = 'dl'
-KEY_DW:str = 'dw'
+KEY_DP:str = 'dp' # -- momentum
+KEY_DL:str = 'dl' # -- length
+KEY_DW:str = 'dw' # -- angle
 
-KEY_KN:str = 'kn'
-KEY_KS:str = 'ks'
+KEY_KN:str = 'kn' # -- normal quadrupole strength
+KEY_KS:str = 'ks' # -- skew quadrupole strenght
 
-KEY_MS:str = 'ms'
-KEY_MO:str = 'mo'
+KEY_MS:str = 'ms' # -- sextupole strength
+KEY_MO:str = 'mo' # -- octupole strength
 
-KEY_E1:str = 'e1'
-KEY_E2:str = 'e2'
+KEY_E1:str = 'e1' # -- dipole entrance wedge angle
+KEY_E2:str = 'e2' # -- dipole exit wedge angle
 
-KEY_DX:str = 'dx'
-KEY_DY:str = 'dy'
-KEY_DZ:str = 'dz'
-KEY_WX:str = 'wx'
-KEY_WY:str = 'wy'
-KEY_WZ:str = 'wz'
+KEY_CX:str = 'cx' # -- horizontal corrector kick
+KEY_CY:str = 'cy' # -- vertical corrector kick
+
+KEY_GX:str = 'gx' # -- horizontal gradient kick
+KEY_GY:str = 'gy' # -- vertical gradient kick
+
+KEY_XX:str = 'xx' # -- xx bmp calibration
+KEY_XY:str = 'xy' # -- xy bmp calibration
+KEY_YX:str = 'yx' # -- yx bmp calibration
+KEY_YY:str = 'yy' # -- yy bmp calibration
+
+KEY_DX:str = 'dx' # -- dx translation alingnment
+KEY_DY:str = 'dy' # -- dy translation alingnment
+KEY_DZ:str = 'dz' # -- dz translation alingnment
+KEY_WX:str = 'wx' # -- wx rotation alingnment
+KEY_WY:str = 'wy' # -- wy rotation alingnment
+KEY_WZ:str = 'wz' # -- wz rotation alingnment
