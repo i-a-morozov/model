d0e36dc79fb2ebb6d4daea0ede8d9f8bcbeb5594
i-a-morozov <i-a-morozov@users.noreply.github.com>
2024-10-02 14:50:39 +0700

    10_02_2024: (command) parametric closed orbit computation

--- model/command/orbit.py
+++ model/command/orbit.py
@@ -11,13 +11,14 @@ from typing import Callable
 import torch
 from torch import Tensor
 
-from ndmap.series import series
+from ndmap.derivative import Table
 from ndmap.propagate import identity
 from ndmap.propagate import propagate
 from ndmap.pfp import fixed_point
 from ndmap.pfp import parametric_fixed_point
 
 from model.library.line import Line
+from model.command.wrapper import group
 
 def orbit(line:Line|Callable,
           guess:Tensor,
@@ -56,7 +57,7 @@ def orbit(line:Line|Callable,
     roots: Optional[Tensor], default=None
         known roots to avoid
     jacobian: Optional[Callable]
-        torch.func.jacfwd (default) or torch.func.jacrev
+        torch.func.jacfwd or torch.func.jacrev (default)
 
     Returns
     -------
@@ -76,3 +77,79 @@ def orbit(line:Line|Callable,
                        solve=solve,
                        roots=roots,
                        jacobian=jacobian)
+
+
+def parametric_orbit(line:Line,
+                     point:Tensor,
+                     parameters:list[Tensor],
+                     *groups:tuple[int, str, list[str]|None, list[str]|None, list[str|None]],
+                     start:Optional[int|str]=None,
+                     alignment:bool=False,
+                     advance:bool=False,
+                     power:int=1,
+                     solve:Optional[Callable]=None,
+                     jacobian:Optional[Callable]=None) -> tuple[Table|list[Table], list[tuple[None, list[str], str]], list[int]]:
+    """
+    Compute parametric closed point (or fixed point)
+
+    Parameters
+    ----------
+    line: Line|Callable
+        input line (one-turn)
+    point: Tensor
+        fixed point
+    parameters: list[Tensor]
+        list of deviation parameters
+    *groups: tuple[int,str,list[str]|None,list[str]|None,list[str|None]]
+        groups specification
+        orders, kinds, names, clean (list of element names)
+    start: Optional[int|str], default=None
+        start element index or name (change start)
+    alignment: bool, default=False
+        flag to include the alignment parameters in the default deviation table
+    advance: bool, default=False
+        flag to advance the parametric orbit over elements or lines
+    power: int, positive, default=1
+        function power / fixed point order
+    solve: Optional[Callable]
+        linear solver(matrix, vector)
+    jacobian: Optional[Callable]
+        torch.func.jacfwd or torch.func.jacrev (default)
+
+    Returns
+    -------
+    tuple[Table|list[Table], list[tuple[None, list[str], str]], list[int]]
+
+
+    """
+    jacobian = torch.func.jacrev if jacobian is None else jacobian
+
+    line = line.clone()
+    if start:
+        line.start = start
+
+    orders, *groups = zip(*groups)
+    groups =tuple(zip(*groups))
+
+    mapping, table, _ = group(line, 0, len(line) - 1, *groups, alignment=alignment, root=True)
+
+    orbit = parametric_fixed_point(orders,
+                                   point,
+                                   parameters,
+                                   mapping,
+                                   power=power,
+                                   solve=solve,
+                                   jacobian=jacobian)
+
+    if not advance:
+        return orbit, table, orders
+
+    orbits = []
+    runner = identity((0, *orders), point, parametric=orbit)
+
+    for element in line:
+        mapping, *_ = group(line, element.name, element.name, *groups, alignment=alignment, root=True)
+        runner = propagate((len(point), *map(len, parameters)), (0, *orders), runner, parameters, mapping, jacobian=jacobian)
+        orbits.append(runner)
+
+    return orbits, table, orders
