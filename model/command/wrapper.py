ec4a629b8b0b74d454c09659217e8986a6f0c050
i-a-morozov <i-a-morozov@users.noreply.github.com>
2024-10-02 13:47:41 +0700

    10_02_2024: removed binding of several tensors to a single leaf

--- model/command/wrapper.py
+++ model/command/wrapper.py
@@ -24,9 +24,6 @@ def wrapper(element:Element,
     """
     Generate wrapper function for an element/line
 
-    Note, several tensors can be binded to selected deviation parameters
-    In this case, the final values is given by the sum of the tensors
-
     Parameters
     ----------
     element : Element
@@ -297,12 +294,12 @@ def _update(data:dict,
             if isinstance(value, dict):
                 _update(value, parameter, target, flag=True)
             if key == parameter:
-                data[key] = data[key] + target
+                data[key] = target
                 return
         return
     if name in data:
         if parameter in data[name]:
-            data[name][parameter] = data[name][parameter] + target
+            data[name][parameter] = target
             return
     for key, value in data.items():
         if isinstance(value, dict):
