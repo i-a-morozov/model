0204a287375a4015c300ce18b812c48c355cdc86
i-a-morozov <i-a-morozov@users.noreply.github.com>
2024-10-06 14:28:24 +0700

    10_06_2024: modified dynamical orbit invocation interface

--- model/command/orbit.py
+++ model/command/orbit.py
@@ -6,8 +6,8 @@ Functionality related to (parametric) closed orbit and fixed points computation
 
 Parameters and functions
 
-orbit                : estimate (dynamical) fixed point
-parametric_orbit     : compute parametric closed point (or fixed point)
+orbit                : compute (dynamical) closed orbit
+parametric_orbit     : compute parametric closed orbit (or fixed point)
 
 """
 from typing import Optional
@@ -25,9 +25,15 @@ from ndmap.pfp import parametric_fixed_point
 from model.library.line import Line
 from model.command.wrapper import group
 
-def orbit(line:Line|Callable,
+
+def orbit(line:Line,
           guess:Tensor,
-          *pars: tuple,
+          parameters:list[Tensor],
+          *groups:tuple[str, list[str]|None, list[str]|None, list[str|None]],
+          start:Optional[int|str]=None,
+          alignment:bool=False,
+          advance:bool=False,
+          full:bool=True,
           limit:int=32,
           power:int=1,
           epsilon:float=1.0E-12,
@@ -35,18 +41,29 @@ def orbit(line:Line|Callable,
           alpha:float=0.0,
           solve:Optional[Callable]=None,
           roots:Optional[Tensor]=None,
-          jacobian:Optional[Callable]=None):
+          jacobian:Optional[Callable]=None) -> Tensor:
     """
-    Estimate (dynamical) fixed point
+    Compute (dynamical) closed orbit
 
     Parameters
     ----------
-    line: Line|Callable
-        input line
+    line: Line
+        input line (one - turm)
     guess: Tensor
         initial guess
-    *pars: tuple
-        additional function arguments
+    parameters: list[Tensor]
+        list of deviation parameters
+    *groups: tuple[str,list[str]|None,list[str]|None,list[str|None]]
+        groups specification
+        kinds, names, clean (list of element names)
+    start: Optional[int|str], default=None
+        start element index or name (change start)
+    alignment: bool, default=False
+        flag to include the alignment parameters in the default deviation table
+    advance: bool, default=False
+        flag to advance the parametric orbit over elements or lines
+    full: bool, default=False
+        flag to perform full propagation
     limit: int, positive
         maximum number of newton iterations
     power: int, positive, default=1
@@ -71,17 +88,38 @@ def orbit(line:Line|Callable,
     """
     jacobian:Callable = torch.func.jacrev if jacobian is None else jacobian
 
-    return fixed_point(limit,
-                       line,
-                       guess,
-                       *pars,
-                       power=power,
-                       epsilon=epsilon,
-                       factor=factor,
-                       alpha=alpha,
-                       solve=solve,
-                       roots=roots,
-                       jacobian=jacobian)
+    jacobian = torch.func.jacrev if jacobian is None else jacobian
+
+    line = line.clone()
+    if start:
+        line.start = start
+
+    mapping, table, _ = group(line, 0, len(line) - 1, *groups, alignment=alignment, root=True)
+
+    point = fixed_point(limit,
+                        mapping,
+                        guess,
+                        *parameters,
+                        power=power,
+                        epsilon=epsilon,
+                        factor=factor,
+                        alpha=alpha,
+                        solve=solve,
+                        roots=roots,
+                        jacobian=jacobian)
+
+    if not advance:
+        return point, table
+
+    points = [point]
+
+    *most, last = line.sequence
+    for element in most + full*[last]:
+        mapping, *_ = group(line, element.name, element.name, *groups, alignment=alignment, root=True)
+        point = mapping(point, *parameters)
+        points.append(point)
+
+    return torch.stack(points), table
 
 
 def parametric_orbit(line:Line,
@@ -96,7 +134,7 @@ def parametric_orbit(line:Line,
                      solve:Optional[Callable]=None,
                      jacobian:Optional[Callable]=None) -> tuple[Table|list[Table], list[tuple[None, list[str], str]], list[int]]:
     """
-    Compute parametric closed point (or fixed point)
+    Compute parametric closed orbit (or fixed point)
 
     Parameters
     ----------
@@ -137,7 +175,7 @@ def parametric_orbit(line:Line,
         line.start = start
 
     orders, *groups = zip(*groups)
-    groups =tuple(zip(*groups))
+    groups = tuple(zip(*groups))
 
     mapping, table, _ = group(line, 0, len(line) - 1, *groups, alignment=alignment, root=True)
 
